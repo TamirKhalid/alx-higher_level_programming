50dd85ffd569c25a617dec6bab72bd3d2f2eac08
TamirKhalid <tamir2khalid@gmail.com>
2024-01-07 13:03:29 -0800

    file

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,72 +1,66 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "lists.h"
 
 /**
- * reverse_listint - reverses a linked list
- * @head: pointer to the first node in the list
- *
- * Return: pointer to the first node in the new list
- */
-void reverse_listint(listint_t **head)
+ *  * print_listint - prints all elements of a listint_t list
+ *   * @h: pointer to head of list
+ *    * Return: number of nodes
+ *     */
+size_t print_listint(const listint_t *h)
 {
-	listint_t *prev = NULL;
-	listint_t *current = *head;
-	listint_t *next = NULL;
-
-	while (current)
-	{
-		next = current->next;
-		current->next = prev;
-		prev = current;
-		current = next;
-	}
-
-	*head = prev;
+	    const listint_t *current;
+	        unsigned int n; /* number of nodes */
+		current = h;
+		n = 0;
+		while (current != NULL)
+		{
+			printf("%i\n", current->n);
+			current = current->next;
+			n++;
+		}
+		return (n);
 }
 
 /**
- * is_palindrome - checks if a linked list is a palindrome
- * @head: double pointer to the linked list
- *
- * Return: 1 if it is, 0 if not
- */
-int is_palindrome(listint_t **head)
+ *  * add_nodeint_end - adds a new node at the end of a listint_t list
+ *   * @head: pointer to pointer of first node of listint_t list
+ *    * @n: integer to be included in new node
+ *     * Return: address of the new element or NULL if it fails
+ *      */
+listint_t *add_nodeint_end(listint_t **head, const int n)
 {
-	listint_t *slow = *head, *fast = *head, *temp = *head, *dup = NULL;
-
-	if (*head == NULL || (*head)->next == NULL)
-		return (1);
-
-	while (1)
+	listint_t *new;
+	listint_t *current;
+	current = *head;
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+		return (NULL);
+	new->n = n;
+	new->next = NULL;
+	if (*head == NULL)
+		*head = new;
+	else
 	{
-		fast = fast->next->next;
-		if (!fast)
-		{
-			dup = slow->next;
-			break;
-		}
-		if (!fast->next)
-		{
-			dup = slow->next->next;
-			break;
-		}
-		slow = slow->next;
+		while (current->next != NULL)
+			current = current->next;
+		current->next = new;
 	}
+	return (new);
+}
 
-	reverse_listint(&dup);
-
-	while (dup && temp)
+/**
+ *  * free_listint - frees a listint_t list
+ *   * @head: pointer to list to be freed
+ *    * Return: void
+ *     */
+void free_listint(listint_t *head)
+{
+	listint_t *current;
+	while (head != NULL)
 	{
-		if (temp->n == dup->n)
-		{
-			dup = dup->next;
-			temp = temp->next;
-		}
-		else
-			return (0);
+		current = head;
+		head = head->next;
+		free(current);
 	}
-
-	if (!dup)
-		return (1);
-
-	return (0);
 }
